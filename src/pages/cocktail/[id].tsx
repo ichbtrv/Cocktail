d635451eb21345f4066e85097795907c1c7b9e24
ivch <ivanchebotarev215@gmail.com>
2022-06-02 15:27:26 -0700

    Create dynamic route

---
+++ src/pages/cocktail/[id].tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import CocktailInfo from '@/components/cocktail/CocktailInfo';
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+import { getIngredients } from '@/utils';
+
+import { useGetCocktailByIdQuery } from '../../store/cocktails/api';
+
+const SingleCocktail = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useGetCocktailByIdQuery(id);
+  let ingredients;
+
+  if (data) ingredients = getIngredients(data);
+
+  if (error) {
+    console.error(error);
+    return <p>Oops, something went wrong..</p>;
+  }
+
+  return (
+    <Main meta={<Meta title="Cocktail" description="A Cocktail Library" />}>
+      {data && (
+        <CocktailInfo
+          id={data.idDrink}
+          name={data.strDrink}
+          image={data.strDrinkThumb}
+          info={data.strAlcoholic}
+          glass={data.strGlass}
+          instructions={data.strInstructions}
+          ingredients={ingredients}
+        />
+      )}
+    </Main>
+  );
+};
+
+export default SingleCocktail;

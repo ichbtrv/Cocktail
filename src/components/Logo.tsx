e22a08e67dc083dc7950dfee2f7449967e7c2b17
ivch <ivanchebotarev215@gmail.com>
2022-06-01 19:43:23 -0700

    Created Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface LogoVariant {
+  variant?: string;
+  width?: string;
+  clickHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Logo: React.FC<LogoVariant> = ({
+  variant = 'cocktail.jpg',
+  width = 'w-36',
+  clickHandler,
+}) => {
+  return (
+    <section
+      onClick={clickHandler}
+      className="mt-1 mr-2 cursor-pointer rounded-xl p-2 mix-blend-screen hover:bg-gray-400 hover:bg-opacity-20"
+    >
+      <img src={`/logos/${variant}`} alt="cocktail" className={`${width}`} />
+    </section>
+  );
+};
+
+export default Logo;

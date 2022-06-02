cc411e393c048c2814a164fa9153a413c533d750
ivch <ivanchebotarev215@gmail.com>
2022-06-01 19:08:51 -0700

    Created Icon file

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,31 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+
+interface IconKey {
+  iconKey: string;
+}
+type Icons = {
+  search: ReactElement;
+};
+
+const icons: Icons = {
+  search: (
+    <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+};
+
+const Icon: React.FC<IconKey> = ({ iconKey }) => {
+  return (
+    <>{icons[iconKey as keyof Icons] ? icons[iconKey as keyof Icons] : ''}</>
+  );
+};
+
+export default Icon;
